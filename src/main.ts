b3fa86be60eb9501cc1e7d7ba119a9860099a793
Jared Cosulich <jared.cosulich@gmail.com>
2023-05-23 07:17:42 -0400

    upgrading ethos-connect

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { EthosConnectPlugin, type EthosConfiguration } from "ethos-connect-vue";
+import {
+  EthosConnectPlugin,
+  type EthosConfiguration,
+  Chain,
+} from "ethos-connect-vue";
 import "./assets/main.css";
 
 const config: EthosConfiguration = {
   apiKey: "8b6347aa-c5fb-460a-8fcc-efeb277f76fc",
+  preferredWallets: ["Ethos Wallet"],
+  chain: Chain.SUI_TESTNET,
 };
 
 createApp(App).use(EthosConnectPlugin, config).mount("#app");
